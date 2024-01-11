2c853055c2e5bd136a0d8f3058a71a2f5c29109d
KevinCowleys <kevincowleys@gmail.com>
2024-01-11 18:26:30 +0000

    Fix for astro-icon not working on prod build

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,25 @@
-export const config = {
+interface ConfigLayout {
+    github: {
+        title: string,
+        href: string,
+        iconName: "GitHub" | "LinkedIn"
+    },
+    linkedin: {
+        title: string,
+        href: string,
+        iconName: "GitHub" | "LinkedIn"
+    }
+};
+
+export const config: ConfigLayout = {
     github: {
         title: "GitHub",
         href: "https://github.com/KevinCowleys",
+        iconName: "GitHub"
     },
     linkedin: {
         title: "LinkedIn",
         href: "https://www.linkedin.com/in/kevincowleys/",
+        iconName: "LinkedIn"
     },
 }

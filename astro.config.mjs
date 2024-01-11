548a9de81cb206f8fa8287f816a1a0822c629ea9
KevinCowleys <kevincowleys@gmail.com>
2024-01-11 17:08:42 +0000

    Images, robots and sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://cowley.dev",
+  site: "https://www.cowley.dev",
   output: "server",
   adapter: cloudflare(),
-  integrations: [icon(), tailwind()]
+  integrations: [icon(), tailwind(), sitemap(), robotsTxt()]
 });
---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+    github: {
+        title: "GitHub",
+        href: "https://github.com/KevinCowleys",
+    },
+    linkedin: {
+        title: "LinkedIn",
+        href: "https://www.linkedin.com/in/kevincowleys/",
+    },
+}
